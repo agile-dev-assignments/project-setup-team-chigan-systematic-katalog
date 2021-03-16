94185b3739ec6cc70174f33364d8dceb39996c59
elizabethlee13 <ejl514@nyu.edu>
2021-03-15 20:19:11 -0400

    Implementing search bar with text and home page from user stori #1

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -2,30 +2,47 @@ import React from 'react'
 // import logo from './logo.svg';
 import './Home.css'
 
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import SearchIcon from "@material-ui/icons/Search";
+import TextField from "@material-ui/core/TextField";
+import {Grid} from "@material-ui/core";
+
 const Home = (props) => {
+
+
+
   return (
+
     <div className="Home">
-      <h1>Welcome!</h1>
+      <img alt="logo" src="https://picsum.photos/200?page=home" />
+      <h1>katalog</h1>
+
+      <Grid container direction="column" alignItems="center" justify="center">
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          label="Search..."
+          margin = "normal"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="start">
+                <IconButton>
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+          )
+        }}
+          />
+        </Grid>
+
       <section className="main-content">
-        <img alt="welcome!" src="https://picsum.photos/200?page=home" />
-        <p>
-          Brownie toffee jujubes tiramisu sugar plum macaroon wafer danish
-          icing. Cotton candy jelly-o topping ice cream tart oat cake
-          gingerbread jelly halvah. Marshmallow gummies danish jelly-o sesame
-          snaps fruitcake candy chocolate bar. Sweet pie sweet powder ice cream
-          cookie apple pie. Powder chocolate bear claw candy lemon drops jelly.
-          Biscuit halvah cookie. Gummi bears biscuit oat cake sweet roll sugar
-          plum brownie candy brownie powder. Halvah halvah candy pastry gummies
-          cotton candy tiramisu jelly-o jelly. Pudding tiramisu gummies caramels
-          danish sugar plum fruitcake apple pie. Chocolate bar sweet toffee
-          gummi bears dessert dragée. Tart croissant pudding sugar plum apple
-          pie brownie biscuit. Candy lollipop liquorice caramels icing biscuit.
-          Marzipan candy canes gingerbread bonbon tart dessert pudding sesame
-          snaps sweet roll. Gingerbread gummi bears toffee chupa chups.
-        </p>
+
       </section>
     </div>
   )
 }
+
+
 
 export default Home
