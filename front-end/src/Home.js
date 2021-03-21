88d635aefec145229395287e7838bda6bb9ca8e1
elizabethlee13 <ejl514@nyu.edu>
2021-03-21 17:46:51 -0400

    Added trending and newly added on home page from user story #45

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,7 +1,11 @@
-import React from 'react'
+
 // import logo from './logo.svg';
 import './Home.css'
-
+import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import './AnimalsList.css'
+import AnimalPreview from './AnimalPreview'
 import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import SearchIcon from "@material-ui/icons/Search";
@@ -12,15 +16,18 @@ import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootst
 import { Link } from 'react-router-dom';
 
 const Home = (props) => {
+  const [data, setData] = useState([])
   return (
     <div>
 
     <div className="Home">
-      <img alt="logo" src="https://picsum.photos/200?page=home" />
+      <img alt="logo" src={logo} />
       <h1>katalog</h1>
 
       <Grid container direction="column" alignItems="center" justify="center">
-
+      <p style={{
+            'white-space': 'pre-wrap'
+            }}>{" \n "}</p>
       <Button style={{alignSelf:'center',width:100,backgroundColor:'#F4F4ED'}}>
                     Login/Signup
       </Button>
@@ -43,18 +50,104 @@ const Home = (props) => {
         }}
           />
 
-        <h3>Trending</h3>
-        
+            <div className="Trending">
+            <p style={{
+            'white-space': 'pre-wrap'
+            }}>{" \n \n \n "}</p>
+              <h3> Top 5 Trending Photocards</h3>
+              <section className="main-content">
+                <img alt="Photocard 1" src="images/image1.jpg" style={{
+            resizeMode: "repeat",
+            display: "flex",
+            padding: 1,
+            height: 100,
+            width: 100
+          }}/>
+                <img alt="Photocard 2" src="images/image3.jpg"style={{
+            resizeMode: "repeat",
+            display: "flex",
+            padding: 1,
+            height: 100,
+            width: 100
+          }} />
+                <img alt="Photocard 3" src="images/image4.jpg"style={{
+            resizeMode: "repeat",
+            display: "flex",
+            padding: 1,
+            height: 100,
+            width: 100
+          }}/>
+                          <img alt="Photocard 4" src="images/image4.jpg"style={{
+            resizeMode: "repeat",
+            display: "flex",
+            padding: 1,
+            height: 100,
+            width: 100
+          }}/>
+                          <img alt="Photocard 5" src="images/image4.jpg"style={{
+            resizeMode: "repeat",
+            display: "flex",
+            padding: 1,
+            height: 100,
+            width: 100
+          }}/>
+
+          </section>
+            </div>
+
+            <div className="Newly Added">
+            <p style={{
+            'white-space': 'pre-wrap'
+            }}>{" \n \n "}</p>
+              <h3> Newly Added</h3>
+              <section className="main-content">
+                <img alt="Photocard 1" src="images/image1.jpg" style={{
+            resizeMode: "repeat",
+            display: "flex",
+            padding: 1,
+            height: 100,
+            width: 100
+          }}/>
+                <img alt="Photocard 2" src="images/image3.jpg"style={{
+            resizeMode: "repeat",
+            display: "flex",
+            padding: 1,
+            height: 100,
+            width: 100
+          }} />
+                <img alt="Photocard 3" src="images/image4.jpg"style={{
+            resizeMode: "repeat",
+            display: "flex",
+            padding: 1,
+            height: 100,
+            width: 100
+          }}/>
+                          <img alt="Photocard 4" src="images/image4.jpg"style={{
+            resizeMode: "repeat",
+            display: "flex",
+            padding: 1,
+            height: 100,
+            width: 100
+          }}/>
+                          <img alt="Photocard 5" src="images/image4.jpg"style={{
+            resizeMode: "repeat",
+            display: "flex",
+            padding: 1,
+            height: 100,
+            width: 100
+          }}/>
+
+          </section>
+            </div>
         </Grid>
-
-      <section className="main-content">
-      </section>
     </div>
-
-
   </div>
   )
 }
+
+
+
+
 
 
 export default Home
