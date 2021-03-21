74158ba8a43f67d96fe8726f01044d4f84ede312
Lee Boodoo <LeeBoodoo54@gmail.com>
2021-03-21 12:56:48 -0400

    Search and results front end implemented, addition to home page

--- front-end/src/Results.js
+++ front-end/src/Results.js
@@ -1,13 +1,15 @@
 import React from 'react'
 // import logo from './logo.svg';
 import './Results.css'
-import Photocard from './Photocard'
+import Photocard from './PhotocardPage'
 
 import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import SearchIcon from "@material-ui/icons/Search";
 import TextField from "@material-ui/core/TextField";
 import {Grid} from "@material-ui/core"
+import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import PhotocardPage from './PhotocardPage';
 
 
 
@@ -15,8 +17,13 @@ const Results = (props) => {
   return (
     <div className="Results">
 
-      <h1>Search</h1> 
+      <section className="above search">
+        <p id = "resultstext">Results</p>
+        <button id = "filterstext">Filters</button>
+      </section>
+      
       <Grid container direction="column" alignItems="left" justify="left">
+
         <TextField
           id="outlined-basic"
           variant="outlined"
@@ -32,12 +39,30 @@ const Results = (props) => {
           )
         }}
           />
+        
         </Grid>
+        <p> Filter 1 <a></a> Filter 2 <a></a> Filter 3</p>
 
+      <section id="main-content">
+        <img alt="Photocard 1" src="images/image1.jpg" />
+        <p4>Member<br />Album<br />Group</p4>
 
-      <section className="main-content">
-        <img alt="Photocard 1" src="https://picsum.photos/200?page=search" />
-        <Photocard />
+      </section>
+      
+      <section id="middle-content"></section>
+
+      <section id="main-content">
+        <img alt="Photocard 2" src="images/image3.jpg" />
+        <p4>Member<br />Album<br />Group</p4>
+
+      </section>
+
+      <section id="middle-content"></section>
+
+      <section id="main-content">
+        <img alt="Photocard 3" src="images/image4.jpg" />
+        <p4>Member <br /> Album <br /> Group</p4>
+
       </section>
     </div>
   )
