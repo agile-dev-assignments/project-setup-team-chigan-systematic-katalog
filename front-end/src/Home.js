f1fe6b3cd5175997fb7c57ec2d7f0a9e528c4a22
elizabethlee13 <ejl514@nyu.edu>
2021-04-01 17:44:33 -0400

    Linking trending section on home to photocards for task 66

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -3,9 +3,8 @@
 import './Home.css'
 // import logo from './logo.svg';
 import React, { useState, useEffect } from 'react'
-// import axios from 'axios'
-import './AnimalsList.css'
-// import AnimalPreview from './AnimalPreview'
+import axios from 'axios'
+import TrendingPreview from './TrendingPreview'
 import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import SearchIcon from "@material-ui/icons/Search";
@@ -19,6 +18,32 @@ const logo = ["logo.png"]
 
 const Home = (props) => {
   const [data, setData] = useState([])
+
+  useEffect(() => {
+    console.log('fetching photocards...')
+    axios("https://my.api.mockaroo.com/photocard.json?key=49083ca0")
+      .then((response) => {
+        setData(response.data)
+      })
+      .catch((err) => {
+        console.log(`No more requests allowed today!`)
+        console.error(err) 
+        const backupData = [
+          {
+            id: 1,
+            name: 'BTS V',
+            price: '$10.51'
+          },
+          {
+            id: 2,
+            name: 'BTS Jimin',
+            price: '$12'
+          },
+        ]
+        setData(backupData)
+      })
+  }, []) 
+
   return (
     <div>
 
@@ -52,58 +77,15 @@ const Home = (props) => {
           <CategoriesModal />
 
             <div className="Trending">
+
             <p style={{
             'white-space': 'pre-wrap'
             }}>{" \n \n \n "}</p>
               <h3> Top 5 Trending Photocards</h3>
               <section className="main-content">
-                <img alt="Photocard 1" src="images/image1.jpg" style={{
-            resizeMode: "repeat",
-            display: "flex",
-            padding: 1,
-            borderWidth: 3,
-            borderColor: '#B388EB',
-            height: 100,
-            width: 100
-          }}/>
-            <img alt="Photocard 2" src="images/image3.jpg"style={{
-            resizeMode: "repeat",
-            display: "flex",
-            padding: 1,
-            borderWidth: 5,
-            borderColor: '#B388EB',
-            height: 100,
-
-            width: 100
-          }} />
-            <img alt="Photocard 3" src="images/image4.jpg"style={{
-            resizeMode: "repeat",
-            display: "flex",
-            padding: 1,
-            borderWidth: 5,
-            borderColor: '#B388EB',
-            height: 100,
-            width: 100
-          }}/>
-            <img alt="Photocard 4" src="images/image4.jpg"style={{
-            resizeMode: "repeat",
-            display: "flex",
-            padding: 1,
-            borderWidth: 5,
-            borderColor: '#B388EB',
-            height: 100,
-            width: 100
-          }}/>
-            <img alt="Photocard 5" src="images/image4.jpg"style={{
-            resizeMode: "repeat",
-            display: "flex",
-            padding: 1,
-            borderWidth: 5,
-            borderColor: '#B388EB',
-            height: 100,
-            width: 100
-          }}/>
-
+                {data.map((item) => (
+                  <TrendingPreview key={item.id} details={item} />
+                ))}
           </section>
             </div>
 
---
+++ front-end/src/TrendingPreview.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './TrendingPreview.css'
+
+const TrendingPreview = (props) => {
+
+  const imgSrc = `https://picsum.photos/200?id=${props.details.id}` // tack on this animal's id to the query
+
+  return (
+    <article className="TrendingPreview">
+      <Link to={`/photocard/${props.details.id}`}>
+        <img alt={props.details.title} src={imgSrc} />
+        <h2>{props.details.title}</h2>
+      </Link>
+    </article>
+  )
+}
+
+export default TrendingPreview
