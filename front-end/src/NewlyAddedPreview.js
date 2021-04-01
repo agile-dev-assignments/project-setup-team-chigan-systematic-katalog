1e5c6ee94d71fc56bee077b1f75c82984e4e8ad6
elizabethlee13 <ejl514@nyu.edu>
2021-04-01 18:06:20 -0400

    Linked newly added on home for task 69

--- front-end/src/NewlyAddedPreview.js
+++ front-end/src/NewlyAddedPreview.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './TrendingPreview.css'
+import './NewlyAddedPreview.css'
 
-const TrendingPreview = (props) => {
+const NewlyAddedPreview = (props) => {
 
-  const imgSrc = `https://picsum.photos/200?id=${props.details.id}` 
+  const imgSrc = `https://picsum.photos/100?id=${props.details.id}` 
 
   return (
-    <article className="TrendingPreview">
+    <article className="NewlyAddedPreview">
       <Link to={`/photocard/${props.details.id}`}>
         <img alt={props.details.title} src={imgSrc} />
         <h2>{props.details.title}</h2>
@@ -16,4 +16,4 @@ const TrendingPreview = (props) => {
   )
 }
 
-export default TrendingPreview
+export default NewlyAddedPreview
