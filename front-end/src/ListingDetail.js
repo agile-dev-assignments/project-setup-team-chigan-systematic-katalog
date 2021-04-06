dc27951b4856e3950002584e585cfb739bf1fddd
Juana Ng <juanangnyc@gmail.com>
2021-04-06 15:37:50 -0400

    made listing page mobile friendly

--- front-end/src/ListingDetail.js
+++ front-end/src/ListingDetail.js
@@ -7,29 +7,34 @@ const ListingDetail = () => {
     
     return (
         <div>
-            <div className="firstColumn">
-                <AliceCarousel disableButtonsControls>
-                    <div>
-                        <img src={"https://picsum.photos/200/325?id=1"} alt="first image" className="sliderimg"/>
+            <div className="top">
+                <div className="firstColumnListing">
+                    <AliceCarousel disableButtonsControls>
+                        <div>
+                            <img src={"https://picsum.photos/200/325?id=1"} alt="first image" className="sliderimg"/>
+                        </div>
+                        <div >
+                            <img src={"https://picsum.photos/200/325?id=2"} alt="second image" className="sliderimg"/>
+                        </div>
+                    </AliceCarousel>
+                </div>
+                <div className="secondColumnListing">
+                    <h3 className="photocardTitle">Photocard Name</h3>
+                    <hr className="line"></hr>
+                    <div className="details">
+                        <h5>Details</h5>
+                        <p>For ...</p>
+                        <p>Price: </p>
+                        <p>Shipping: </p>
+                        <p>Location: </p>
+                        <p>Ship To: </p>
+                        <p>Posted: </p>
                     </div>
-                    <div >
-                        <img src={"https://picsum.photos/200/325?id=2"} alt="second image" className="sliderimg"/>
-                    </div>
-                </AliceCarousel>
+                    <hr className="line"></hr>
+                </div>
             </div>
-            <h3 className="photocardTitle">Photocard</h3>
-            <hr className="line"></hr>
-            <div className="details">
-                <h5>Details</h5>
-                <p>Price: </p>
-                <p>Shipping: </p>
-                <p>Location: </p>
-                <p>Ship To: </p>
-                <p>Posted: </p>
-            </div>
-            <hr className="line"></hr>
             <div className="description">
-                <p>Hello this is a description</p>
+                <p>Description</p>
             </div>
             <hr className="line"></hr>
             <div className="profileLink">
