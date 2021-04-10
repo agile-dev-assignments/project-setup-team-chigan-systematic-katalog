3ae02556f9beaa9fafe5560720b9d35b05047fff
Juana Ng <juanangnyc@gmail.com>
2021-04-10 14:27:34 -0400

    created unit test for profile route

---
+++ back-end/test/profile.spec.js
@@ -0,0 +1,12 @@
+const request = require('supertest')
+
+const app = require('../server')
+
+describe('the Profile route', function(){
+    it('profile route should return json', function(done){
+        request(app)
+            .get('/profile')
+            .expect('Content-Type', /json/)
+            .expect(200, done)
+    })
+})
