05753504053b263e9f7b5ca5cab7fb6c18c50004
Rachel Lin <rclin98@gmail.com>
2021-04-20 22:06:02 -0400

    adjusted font size for trending and newly added

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,43 +1,26 @@
 
 // import logo from './logo.svg';
+// import PhotocardPage from './PhotocardPage'
+// import IconButton from "@material-ui/core/IconButton";
+// import InputAdornment from "@material-ui/core/InputAdornment";
+// import SearchIcon from "@material-ui/icons/Search";
+// import TextField from "@material-ui/core/TextField";
+// import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+// import { Link } from 'react-router-dom';
+
 import './Home.css'
-// import logo from './logo.svg';
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import axios from 'axios'
 import TrendingPreview from './TrendingPreview'
-import PhotocardPage from './PhotocardPage'
 import NewlyAddedPreview from './NewlyAddedPreview'
-import IconButton from "@material-ui/core/IconButton";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import SearchIcon from "@material-ui/icons/Search";
-import TextField from "@material-ui/core/TextField";
 import {Grid} from "@material-ui/core";
-import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import CategoriesModal from './CategoriesModal'
-import { Link } from 'react-router-dom';
 
 const logo = ["logo.png"]
 
 const Home = (props) => {
   const [data, setData] = React.useState([])
-
-  const testData = {
-    id: 1,
-    photocard_name: "Bang Chan Double Sided #2 Photocard",
-    group: "Stray Kids",
-    member: "Bang Chan",
-    album: "GO生(GO LIVE)",
-    picture: "images/image1.jpg",
-    picture2: "images/image2.jpg"}
-
-  //useEffect(() => {
-
-  // axios("https://my.api.mockaroo.com/photocard.json?key=49083ca0")
-  //    .then((response) => {
-  //      setData(response.data)
-  //    })
-  // }, []) 
-
+  
   useEffect(() => {
     console.log('fetching photocards...')
     axios.get('http://localhost:4000/photocarddata')
