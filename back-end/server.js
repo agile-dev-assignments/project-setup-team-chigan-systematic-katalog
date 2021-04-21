56f37991316f0408f81ad553450c0762587d2a10
Rachel Lin <rclin98@gmail.com>
2021-04-20 22:49:31 -0400

    commented out authentication code (with errors) in server.js

--- back-end/server.js
+++ back-end/server.js
@@ -9,11 +9,11 @@ const users = []
 const bcrypt = require('bcrypt')
 const passport = require ('passport')
 
-const initializePassport = require('./passport-config')
-initializePassport(
-  passport, 
-  email => user.find(user => user.email === email)
-)
+// const initializePassport = require('./passport-config')
+// initializePassport(
+//   passport, 
+//   email => user.find(user => user.email === email)
+// )
 
 // which port to listen for HTTP(S) requests
 const port = 4000
@@ -23,25 +23,25 @@ const listener = server.listen(port, function () {
   console.log(`Server running on port: ${port}`)
 })
 
-server.post('/login', (req,res) => { //app
-  req.body.email
-})
+// server.post('/login', (req,res) => { //app
+//   req.body.email
+// })
 
-server.post('/signup', (req,res) => { //app
-  try{
-    const hashedPassword = bcrypt.hash(req.body.password, 10)
-    users.push({
-      id: Date.now().toString(),
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPassword
-    })
-    res.redirect('/login')
-  } catch{
-    res.redirect('/signup')
-  }
-  console.log(users)
-})
+// server.post('/signup', (req,res) => { //app
+//   try{
+//     const hashedPassword = bcrypt.hash(req.body.password, 10)
+//     users.push({
+//       id: Date.now().toString(),
+//       username: req.body.username,
+//       email: req.body.email,
+//       password: hashedPassword
+//     })
+//     res.redirect('/login')
+//   } catch{
+//     res.redirect('/signup')
+//   }
+//   console.log(users)
+// })
 
 // a function to stop listening to the port
 const close = () => {
